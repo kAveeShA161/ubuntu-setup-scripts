58ff817c0b584826c08ec049987d3659f59932fc
Kaveesha Sewmini <kaveeshasewmini161@gmail.com>
2026-03-03 20:59:53 +0530

    Added setup_env.sh file

---
+++ setup_env.sh
@@ -0,0 +1,8 @@
+mkdir -p ¬/workspace/secure_data
+touch ¬/workspace/secure_data/config.cfg
+
+chmod 640 ¬/workspace/secure_data/config.cfg
+
+sudo apt update
+sudo apt install nginx -y
+sudo systemctl start nginx
